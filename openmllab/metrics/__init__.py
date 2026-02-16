e5986c3e7abab9db1788c4407adb064ae1331e6a
Yash Khedekar <yashkhedekar31@gmail.com>
2026-02-16 23:44:25 +0530

    Feat: make metrices accessible through import

---
+++ openmllab/metrics/__init__.py
@@ -0,0 +1,12 @@
+from ._regression import (
+    mean_absolute_error,
+    mean_squared_error,
+    root_mean_squared_error
+)
+
+from ._classification import (
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score
+)
