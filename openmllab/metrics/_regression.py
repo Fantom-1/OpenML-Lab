96f64bef121f2ac404146e5fd36c95cff17a5589
Yash Khedekar <yashkhedekar31@gmail.com>
2026-03-15 00:29:04 +0530

    Feat: Implement F1 Score

--- openmllab/metrics/_regression.py
+++ openmllab/metrics/_regression.py
@@ -10,3 +10,14 @@ def mean_squared_error(y_true, y_pred):
 
 def root_mean_squared_error(y_true, y_pred):
     return np.sqrt(mean_squared_error(y_true, y_pred))
+
+
+def r2_score(y_true, y_pred):
+    y_true, y_pred = np.asarray(y_true), np.asarray(y_pred)
+    numerator = np.sum((y_true - y_pred) ** 2)
+    denominator = np.sum((y_true - np.mean(y_true)) ** 2)
+    
+    if denominator == 0:
+        return 0.0 
+    
+    return 1 - (numerator / denominator)
