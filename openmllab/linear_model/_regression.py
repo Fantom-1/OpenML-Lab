e2be0697ca7e83d46722712a99258967114ef52d
Yash Khedekar <yashkhedekar31@gmail.com>
2026-03-15 22:44:53 +0530

    Feat: Implement Lasso(L1) Reqularization

--- openmllab/linear_model/_regression.py
+++ openmllab/linear_model/_regression.py
@@ -11,7 +11,6 @@ class LinearRegression(BaseEstimator):
 
     def fit(self, X, y):
         X, y = np.asarray(X), np.asarray(y)
-        # The Bias Trick: Adding a column of 1s
         X_b = np.c_[np.ones((X.shape[0], 1)), X]
         # Normal Equation: theta = (X^T @ X)^-1 @ X^T @ y
         # We use pinv (pseudo-inverse) for numerical stability
@@ -73,3 +72,30 @@ class RidgeRegression(BaseEstimator):
         X = np.asarray(X)
         X_b = np.c_[np.ones((X.shape[0], 1)), X]
         return X_b @ self.theta
+
+class LassoRegression(BaseEstimator):
+    def __init__(self, alpha=1.0, lr=0.01, epochs=1000):
+        self.alpha = alpha
+        self.lr = lr
+        self.epochs = epochs
+        self.weights = None
+        self.bias = 0
+
+    def fit(self, X, y):
+        X, y = np.asarray(X), np.asarray(y)
+        n_samples, n_features = X.shape
+        self.weights = np.zeros(n_features)
+        
+        for _ in range(self.epochs):
+            y_pred = np.dot(X, self.weights) + self.bias
+            error = y_pred - y
+            
+            dw = (2/n_samples) * np.dot(X.T, error) + self.alpha * np.sign(self.weights)
+            db = (2/n_samples) * np.sum(error)
+            
+            self.weights -= self.lr * dw
+            self.bias -= self.lr * db
+
+    def predict(self, X):
+        X = np.asarray(X)
+        return np.dot(X, self.weights) + self.bias
